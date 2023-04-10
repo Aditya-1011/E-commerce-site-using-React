f23b7cad4a7fd822310b73e135ca0276f154011f
Aditya-1011 <rawataditya769@gmail.com>
2023-04-11 00:27:37 +0530

    comm

--- src/Pages/NAV.jsx
+++ src/Pages/NAV.jsx
@@ -1,5 +1,11 @@
+
+import { Badge } from '@material-ui/core'
+import { ShoppingCartOutlined } from '@material-ui/icons'
+
 import React from 'react'
 import styled from 'styled-components'
+
+
 
 
 const Container=styled.div`
@@ -11,24 +17,61 @@ const Wrapper=styled.div`
 padding: 10px 20px;
 display:flex;
 justify-content: space-between;
+align-items:center;
+
 `
 const Left=styled.div`
 flex:1;
+display:flex;
+align-items:center;
+
 
 
 `
 const Center=styled.div`
 flex:1;
+text-align:center;
+
 
 `
 const Right=styled.div`
 flex:1;
-${'' /* ADITYA */}
+display:flex;
+justify-content:flex-end;
+align-item:center;
+
+
+
+`
+const MenuItem=styled.div`
+font-size:14;
+cursor:pointer;
+margin-left:20px;
 
 `
 const Language=styled.span`
-font-size:30px;
+font-size:15px;
 pointer: cursor:
+`
+const Input=styled.input`
+border:none;
+
+
+`
+const Logo=styled.h1`
+font-weight:bold;
+`
+const SearchContainer=styled.div`
+  border:0.5px solid lightgrey;
+  display:flex;
+  align-item:center;
+  margin-Left:25px;
+  padding:5px;
+`
+const Search=styled.div`
+
+
+
 `
 export const NAV = () => {
   return (
@@ -36,15 +79,24 @@ export const NAV = () => {
     <Wrapper>
     <Left>
     <Language>
-        ENGLish
+        En
     </Language>
     <SearchContainer>
-        input
-        icon
+        <Input/>
+        <Search style={{color:"gray",fontSize:16}}/>
     </SearchContainer>
     </Left>
-    <Center>SDFSDFA</Center>
-    <Right>SDFSDF</Right>
+    <Center><Logo>Buy Clothes</Logo></Center>
+    <Right ><MenuItem>Register</MenuItem>
+    <MenuItem>Sign In</MenuItem>
+    <MenuItem>
+    <Badge badgeContent={4} color="primary">
+      <ShoppingCartOutlined />
+    </Badge>
+    </MenuItem>
+      
+
+    </Right>
     
     
         
